2e19df887bf1ba1a3d9445ab58e2a906e75b0cd1
bilakispa <bilakispa@gmail.com>
2015-11-28 17:30:47 +0200

    Added simple enemy spawner script.

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+	//TO DO
+	/*public enum EnemyType {
+		ChickenLeg, KetchupBottle
+	}
+
+	public EnemyType enemyType; */
+
+	public GameObject enemyPrefab;
+	//add spawn params
+	// Use this for initialization
+	void Start () {
+		Instantiate (enemyPrefab, transform.position, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
