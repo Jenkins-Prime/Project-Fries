de19580a19e4d6cb2327120cafeccfa5488f4373
bilakispa <bilakispa@gmail.com>
2015-12-19 01:46:37 +0200

    ShootProjectile: Added holding the shoot button to change the angle of the projectile.

--- Assets/Scripts/Player/ShootProjectile.cs
+++ Assets/Scripts/Player/ShootProjectile.cs
@@ -5,28 +5,37 @@ public class ShootProjectile : MonoBehaviour {
 	GameObject projectile;
 	public GameObject[] projectileList;
 	public Vector3 target;
-	public float shootAngle = 30f;
+	float currentAngle;
+	public float initialAngle = 30f;
+	public float maxAngle = 80f;
 	public float shootTorque = -15f;
 	Rigidbody2D rb2D;
 
 	// Use this for initialization
 	void Start () {
 		target = transform.right * 4;
+		currentAngle = initialAngle;
 		rb2D = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown (KeyCode.F) && projectile == null)
-		{
-			SpawnProjectile(Random.Range(0, projectileList.Length));
+		if (projectile == null) {
+			if(Input.GetButton("Attack") ) {
+				if(currentAngle < maxAngle) {
+					currentAngle += 1;
+				}
+			} else if (Input.GetButtonUp("Attack")){
+				SpawnProjectile(Random.Range(0, projectileList.Length));
+				currentAngle = initialAngle;
+			}
 		}
 	}
 
 	void SpawnProjectile(int i) {
 		projectile = (GameObject)Instantiate(projectileList[i], transform.position, Quaternion.identity);
 		projectile.transform.parent = transform; //set the parent as the player
-		projectile.GetComponent<Rigidbody2D> ().velocity = ProjectionVelocity.Calculate (target, shootAngle) + rb2D.velocity;
+		projectile.GetComponent<Rigidbody2D> ().velocity = ProjectionVelocity.Calculate (target, currentAngle) + rb2D.velocity;
 		projectile.GetComponent<Rigidbody2D> ().AddTorque (shootTorque);
 		Destroy(projectile, 3);
 	}
