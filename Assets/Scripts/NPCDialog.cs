16c37d7a105907716aa6116ad3b8c3d1bcfbd11f
bilakispa <bilakispa@gmail.com>
2016-02-08 11:34:16 +0200

    Added ui to dialog system

--- Assets/Scripts/NPCDialog.cs
+++ Assets/Scripts/NPCDialog.cs
@@ -1,39 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class NPCDialog : MonoBehaviour {
+	bool canShowDialog;
 	bool dialogEnabled;
-	public string dialogText = "This is an example dialog text, fix me pls. :(";
+	Image dialogBox;
+	Text dialogText;
+	public string lines = "This is an example dialog text, fix me pls. :(";
 
 	// Use this for initialization
 	void Start () {
+		canShowDialog = false;
 		dialogEnabled = false;
 		transform.GetChild(0).gameObject.SetActive(false);
+		dialogBox = GameObject.FindGameObjectWithTag ("DialogBox").GetComponent<Image>();
+		dialogText = GameObject.FindGameObjectWithTag ("DialogText").GetComponent<Text>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (dialogEnabled) {
+		if (canShowDialog) {
 			if(Input.GetButtonDown("Submit")) {
-				//Disable Movement
-				//Pause Time, Maybe?
-				//Show Dialog
-				//Import from file
-				Debug.Log (dialogText);
+				if(!dialogEnabled) {
+					Time.timeScale = 0; //"Raw" method of pausing the game, not sure if it's good practice to use.
+					//TO DO: Import from file
+					dialogText.text = lines;
+					dialogBox.enabled = true;
+					dialogText.enabled = true;
+					dialogEnabled = true;
+				} else if(dialogEnabled) {
+					Time.timeScale = 1; //Unpause
+					dialogBox.enabled = false;
+					dialogText.enabled = false;
+					dialogEnabled = false;
+				}
 			}
 		}
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.tag == "Player") {
-			dialogEnabled = true;
+			canShowDialog = true;
 			transform.GetChild(0).gameObject.SetActive(true);
 		}
 	}
 
 	void OnTriggerExit2D(Collider2D other) {
 		if (other.tag == "Player") {
-			dialogEnabled = false;
+			canShowDialog = false;
 			transform.GetChild(0).gameObject.SetActive(false);
 		}
 	}
