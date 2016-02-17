47f8d06b0635c59c04afdf2e265cc4b6faccd227
joshmond <joshmond@ymail.com>
2016-02-17 01:08:42 +0000

    Modified the dialogue system, walk up to the sign and press <Enter> to interact and press and hold <Space> to make the text go faster.

--- Assets/Scripts/NPCDialog.cs
+++ Assets/Scripts/NPCDialog.cs
@@ -1,55 +1,84 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 
-public class NPCDialog : MonoBehaviour {
-	bool canShowDialog;
-	bool dialogEnabled;
-	Image dialogBox;
-	Text dialogText;
+public class NPCDialog : MonoBehaviour 
+{
 	public string lines = "This is an example dialog text, fix me pls. :(";
+	private bool inRange;
+	private UIManager manager;
+	private SpriteRenderer speechBubble;
+	private Text dialogueText;
+	private float letterPause;
+	private bool isShowing;
 
-	// Use this for initialization
-	void Start () {
-		canShowDialog = false;
-		dialogEnabled = false;
-		transform.GetChild(0).gameObject.SetActive(false);
-		dialogBox = GameObject.FindGameObjectWithTag ("DialogBox").GetComponent<Image>();
-		dialogText = GameObject.FindGameObjectWithTag ("DialogText").GetComponent<Text>();
+	void Awake()
+	{
+		manager = GameObject.FindGameObjectWithTag ("Game UI").GetComponent<UIManager> ();
+		speechBubble = gameObject.transform.GetChild (0).GetComponent<SpriteRenderer> ();
+		dialogueText = GameObject.FindGameObjectWithTag ("Game UI").transform.GetChild (0).GetChild (0).GetComponent<Text> ();
+		isShowing = false;
 	}
-	
+
+	void Start()
+	{
+		speechBubble.enabled = false;
+		letterPause = 0.1f;
+		//dialogueText.text = lines;
+	}
 	// Update is called once per frame
-	void Update () {
-		if (canShowDialog) {
-			if(Input.GetButtonDown("Submit")) {
-				if(!dialogEnabled) {
-					Time.timeScale = 0; //"Raw" method of pausing the game, not sure if it's good practice to use.
-					//TO DO: Import from file
-					dialogText.text = lines;
-					dialogBox.enabled = true;
-					dialogText.enabled = true;
-					dialogEnabled = true;
-				} else if(dialogEnabled) {
-					Time.timeScale = 1; //Unpause
-					dialogBox.enabled = false;
-					dialogText.enabled = false;
-					dialogEnabled = false;
-				}
+	void Update () 
+	{
+		if(inRange && Input.GetButtonDown("Submit"))
+		{
+			manager.ShowDialogue();
+			StartCoroutine("DisplayDialogue");
+		}
+
+		if(!inRange)
+		{
+			manager.HideDialogue();
+			StopCoroutine("DisplayDialogue");
+			dialogueText.text = "";
+		}
+
+	}
+
+	private IEnumerator DisplayDialogue()
+	{
+		foreach(char letter in lines.ToCharArray())
+		{
+			dialogueText.text += letter;
+
+			if(Input.GetButton("Jump"))
+			{
+				yield return new WaitForSeconds(letterPause * 0.09f);
 			}
+			else
+			{
+				yield return new WaitForSeconds(letterPause);
+			}
+
 		}
 	}
 
-	void OnTriggerEnter2D(Collider2D other) {
-		if (other.tag == "Player") {
-			canShowDialog = true;
-			transform.GetChild(0).gameObject.SetActive(true);
+
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		if (other.tag == "Player") 
+		{
+			inRange = true;
+			speechBubble.enabled = true;
 		}
 	}
 
-	void OnTriggerExit2D(Collider2D other) {
-		if (other.tag == "Player") {
-			canShowDialog = false;
-			transform.GetChild(0).gameObject.SetActive(false);
+	void OnTriggerExit2D(Collider2D other) 
+	{
+		if (other.tag == "Player") 
+		{
+			inRange = false;
+			speechBubble.enabled = false;
+
 		}
 	}
 }
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour 
+{
+	public GameObject dialogueUI;
+
+	// Use this for initialization
+	void Start()
+	{
+		dialogueUI.SetActive (false);
+	}
+
+	public void ShowDialogue()
+	{
+		dialogueUI.SetActive (true);
+	}
+
+	public void HideDialogue()
+	{
+		dialogueUI.SetActive (false);
+	}
+}
