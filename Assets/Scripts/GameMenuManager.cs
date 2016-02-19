520c1353b4bb2481ea9783a73454da5948c760de
joshmond <joshmond@ymail.com>
2016-02-19 18:06:59 +0000

    Updated the dialogue system to only allow interaction once.

---
+++ Assets/Scripts/GameMenuManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class GameMenuManager : MonoBehaviour //IPointerClickHandler
+{
+	public GameObject menuPanel;
+	public GameObject optionsPanel;
+
+	private bool isShowing;
+
+	void Start()
+	{
+		menuPanel.SetActive (false);
+	}
+
+	void Update()
+	{
+		if(Input.GetButtonDown("Cancel"))
+		{
+			isShowing = !isShowing;
+		}
+
+		if(isShowing)
+		{
+			ShowGameWindow();
+		}
+		else
+		{
+			HideGameWindow();
+		}
+	}
+
+	public void ShowGameWindow()
+	{
+		menuPanel.SetActive(true);
+		Time.timeScale = 0.0f;
+	}
+
+	public void HideGameWindow()
+	{
+		menuPanel.SetActive(false);
+		Time.timeScale = 1.0f;
+	}
+
+	public void ShowOptionsWindow()
+	{
+		optionsPanel.SetActive (true);
+	}
+
+	public void HideOptionsWindow()
+	{
+		optionsPanel.SetActive (false);
+	}
+}
--- Assets/Scripts/NPCDialog.cs
+++ Assets/Scripts/NPCDialog.cs
@@ -13,44 +13,40 @@ public class NPCDialog : MonoBehaviour
 	private UIManager manager;
 	private PlayerController playerController;
 	private SpriteRenderer speechBubble;
+	private SpriteRenderer blockInteration;
 	private Text dialogueText;
 	private Image endOfDialogue;
 	private float letterPause;
 	private bool isShowing;
 	private bool isTextComplete;
+	private bool hasInteracted;
 	private GameObject npc;
 	private GameObject player;
 	private GameObject canvas;
-
-	private Toggle slow;
-	private Toggle fast;
-	private Toggle faster;
 
 	void Awake()
 	{
 		manager = GameObject.FindGameObjectWithTag ("Game UI").GetComponent<UIManager> ();
 		speechBubble = gameObject.transform.GetChild (0).GetComponent<SpriteRenderer> ();
+		blockInteration = gameObject.transform.GetChild (1).GetComponent<SpriteRenderer> ();
 		dialogueText = GameObject.FindGameObjectWithTag ("Game UI").transform.GetChild (0).GetChild (0).GetComponent<Text> ();
 		endOfDialogue = GameObject.FindGameObjectWithTag ("Game UI").transform.GetChild (0).GetChild (1).GetComponent<Image> ();
 		player = GameObject.FindGameObjectWithTag("Player");
 		playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
 		canvas = GameObject.FindGameObjectWithTag("Game UI");
-		slow = GameObject.FindGameObjectWithTag ("Dialogue Slow").GetComponent<Toggle> ();
-		fast = GameObject.FindGameObjectWithTag ("Dialogue Fast").GetComponent<Toggle> ();
-		faster = GameObject.FindGameObjectWithTag ("Dialogue Faster").GetComponent<Toggle> ();
 
 	}
 
 	void Start()
 	{
 		speechBubble.enabled = false;
-		letterPause = 0.1f;
+		blockInteration.enabled = false;
 		isShowing = false;
 		isTextComplete = false;
 		endOfDialogue.enabled = false;
-		slow.isOn = true;
-		fast.isOn = false;
-		faster.isOn = false;
+		hasInteracted = false;
+		letterPause = 0.1f;
+
 	}
 
 	// Update is called once per frame
@@ -58,38 +54,49 @@ public class NPCDialog : MonoBehaviour
 	{
 		canvas.transform.position = new Vector3 (player.transform.position.x - 3.0f, player.transform.position.y + 1.0f, player.transform.position.z);
 
-		if(Vector3.Distance(player.transform.position, transform.position) < 1.0f)
+
+		if(hasInteracted)
+		{
+			blockInteration.enabled = true;
+			speechBubble.enabled = false;
+		}
+
+		if(Vector3.Distance(player.transform.position, transform.position) < 1.0f && !hasInteracted)
 		{
 			speechBubble.enabled = true;
 			inRange = true;
 
-			if(Input.GetButtonDown("Submit"))
+			if(Input.GetButtonDown("Submit") && inRange && !isTextComplete)
 			{
 				CanInteract();
 			}
+
+			if(Input.GetButtonDown("Submit") && !hasInteracted && isTextComplete)
+			{
+				playerController.moveSpeed = 5.0f;
+				playerController.jumpHeight = 10.0f;
+				player.GetComponent<Animator>().enabled = true;
+				CloseDialogueBox();
+			}
+
 		}
 		else
 		{
 			inRange = false;
 			speechBubble.enabled = false;
-		}
-			
-		if(isTextComplete && Input.GetButtonDown("Submit") && inRange)
-		{
-			playerController.moveSpeed = 5.0f;
-			playerController.jumpHeight = 10.0f;
-			player.GetComponent<Animator>().enabled = true;
-			CloseDialogueBox();
-		}
 
+		}
 	}
+
+
 
 	private void CloseDialogueBox()
 	{
 		manager.HideDialogue();
 		StopCoroutine("DisplayDialogue");
 		dialogueText.text = "";
-		isShowing = false;
+		isShowing = true;
+		hasInteracted = true;
 	}
 
 	private IEnumerator DisplayDialogue()
@@ -105,33 +112,21 @@ public class NPCDialog : MonoBehaviour
 			}
 			else
 			{
-				isTextComplete = false;
+				//isTextComplete = false;
 				endOfDialogue.enabled = false;
 			}
-
-			if(slow.isOn)
-			{
-				yield return new WaitForSeconds(letterPause);
-			}
-			else if(fast.isOn)
-			{
-				yield return new WaitForSeconds(letterPause * 0.2f);
-			}
-			else if(faster.isOn)
-			{
-				yield return new WaitForSeconds(letterPause * 0.01f);
-			}
+			yield return new WaitForSeconds(letterPause * 0.2f);
 
 		}
 	}
 
 	private void ReadFile()
 	{
-		FileStream stream2 = new FileStream (Application.dataPath + "/Dialogue/Dialogue" + ID + ".txt", FileMode.Open, FileAccess.Read);
+		FileStream stream = new FileStream (Application.dataPath + "/Dialogue/Dialogue" + ID + ".txt", FileMode.Open, FileAccess.Read);
 			
-			using (StreamReader reader2 = new StreamReader(stream2, Encoding.UTF8))
+			using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
 			{
-				dialogue = reader2.ReadToEnd();
+				dialogue = reader.ReadToEnd();
 			}
 	}
 
