9b2e4f7d790701a11594e1c67e581b79b85ff654
joshmond <joshmond@ymail.com>
2016-02-24 23:49:10 +0000

    Implemented over world HUB

---
+++ Assets/Scripts/Hub World/SelectLevel.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SelectLevel : MonoBehaviour 
+{
+	private Text level;
+	private int currentLevel;
+	private SelectWorld selectWorld;
+
+	void Awake()
+	{
+		level = GameObject.FindGameObjectWithTag ("Level Select").GetComponent<Text> ();
+		selectWorld = GameObject.FindGameObjectWithTag ("World Selector").GetComponent<SelectWorld> ();
+	}
+
+	void Start () 
+	{
+		currentLevel = 1;
+	}
+
+	void Update () 
+	{
+		if(currentLevel > 9)
+		{
+			currentLevel = 1;
+		}
+
+		if(currentLevel < 1)
+		{
+			currentLevel = 9;
+		}
+
+		if(currentLevel == 1 && Input.GetButtonDown("Submit"))
+		{
+			Application.LoadLevel(1);
+		}
+
+		LevelSelect ();
+		level.text = currentLevel.ToString ();
+
+	}
+
+	private void LevelSelect()
+	{
+		if(Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			currentLevel++;
+		}
+
+		if(Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			currentLevel--;
+		}
+	}
+}
---
+++ Assets/Scripts/Hub World/SelectWorld.cs
@@ -0,0 +1,131 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class SelectWorld : MonoBehaviour 
+{
+	public GameObject levelSelectOne;
+	public GameObject levelSelectTwo;
+	public GameObject levelSelectThree;
+	public GameObject levelSelectFour;
+
+	[HideInInspector]
+	public bool isWorldOne;
+	[HideInInspector]
+	public bool isWorldTwo;
+	[HideInInspector]
+	public bool isWorldThree;
+	[HideInInspector]
+	public bool isWorldFour;
+	
+	public int currentWorld;
+	private GameObject worldOne;
+	private GameObject worldTwo;
+	private GameObject worldThree;
+	private GameObject worldFour;
+
+	void Awake()
+	{
+		worldOne = GameObject.FindGameObjectWithTag("World One");
+		worldTwo = GameObject.FindGameObjectWithTag("World Two");
+		worldThree = GameObject.FindGameObjectWithTag("World Three");
+		worldFour = GameObject.FindGameObjectWithTag("World Four");
+	}
+	
+	void Start () 
+	{
+		currentWorld = 1;
+		levelSelectOne.SetActive (false);
+		levelSelectTwo.SetActive (false);
+		levelSelectThree.SetActive (false);
+		levelSelectFour.SetActive (false);
+
+		isWorldOne = false;
+		isWorldTwo = false;
+		isWorldThree = false;
+		isWorldFour = false;
+	}
+
+	void Update () 
+	{
+		WorldSelect ();
+
+	}
+
+	private void WorldSelect()
+	{
+		if(Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			currentWorld++;
+		}
+
+		if(Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			currentWorld--;
+
+		}
+
+		
+		if (currentWorld == 1) 
+		{
+
+			if(Input.GetButtonDown ("Submit"))
+			{
+				levelSelectOne.SetActive (true);
+				levelSelectTwo.SetActive (false);
+				levelSelectThree.SetActive (false);
+				levelSelectFour.SetActive (false);
+			}
+			
+			gameObject.transform.position = new Vector3 (worldOne.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+		}
+		else if (currentWorld == 2) 
+		{
+			if(Input.GetButtonDown ("Submit"))
+			{
+				levelSelectOne.SetActive (false);
+				levelSelectTwo.SetActive (true);
+				levelSelectThree.SetActive (false);
+				levelSelectFour.SetActive (false);
+			}
+			
+			gameObject.transform.position = new Vector3(worldTwo.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+		}
+		else if (currentWorld == 3) 
+		{
+			if(Input.GetButtonDown ("Submit"))
+			{
+				isWorldThree = true;
+				levelSelectOne.SetActive (false);
+				levelSelectTwo.SetActive (false);
+				levelSelectThree.SetActive (true);
+				levelSelectFour.SetActive (false);
+			}
+			
+			gameObject.transform.position = new Vector3(worldThree.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+		}
+		else 
+		{
+			if(Input.GetButtonDown ("Submit"))
+			{
+				isWorldFour = true;
+				levelSelectOne.SetActive (false);
+				levelSelectTwo.SetActive (false);
+				levelSelectThree.SetActive (false);
+				levelSelectFour.SetActive (true);
+			}
+			
+			gameObject.transform.position = new Vector3(worldFour.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+		}
+		
+		if(currentWorld > 4)
+		{
+			currentWorld = 1;
+		}
+		
+		if(currentWorld < 1)
+		{
+			currentWorld = 4;
+		}
+	}
+}
