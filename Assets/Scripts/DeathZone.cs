364acb345fbccd09c8b8c782553a2876e580d683
joshmond <joshmond@ymail.com>
2016-02-25 01:51:58 +0000

    Implemented a respawn system

--- Assets/Scripts/DeathZone.cs
+++ Assets/Scripts/DeathZone.cs
@@ -3,23 +3,59 @@ using System.Collections;
 
 public class DeathZone : MonoBehaviour 
 {
+	public GameObject playerObject;
+
 	private GameController gameController;
+	private GameObject player;
+	private GameObject playerCamera;
+	private GameObject spawnPoint;
+	private GameObject cameraPosition;
+	private bool isDead;
+	private CameraController cameraController;
 
 	void Awake()
 	{
 		gameController = GameObject.FindGameObjectWithTag ("GameController").GetComponent<GameController> ();
+		player = GameObject.FindGameObjectWithTag("Player");
+		playerCamera = GameObject.FindGameObjectWithTag("MainCamera");
+		cameraPosition = GameObject.FindGameObjectWithTag("MainCamera");
+		spawnPoint = GameObject.FindGameObjectWithTag("Spawn Point");
+	}
+
+	void Start()
+	{
+		isDead = false;
+	}
+
+	void Update()
+	{
+		if(isDead)
+		{
+			playerCamera.transform.position = Vector3.Lerp(playerCamera.transform.position, new Vector3(spawnPoint.transform.position.x + 10.0f, spawnPoint.transform.position.y, spawnPoint.transform.position.z), 5.0f * Time.deltaTime);
+			StartCoroutine(Respawn (10.0f));
+			player.transform.position = new Vector3(spawnPoint.transform.position.x, 1.0f, 0.0f);
+			StartCoroutine(Respawn (3.0f));
+			player.SetActive(true);
+			isDead = false;
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if(other.tag == "Player")
 		{
-			gameController.Die ();
+			player.SetActive(false);
+			isDead = true;
+		}
+		else
+		{
+			isDead = false;
 		}
 	}
 
-	private void Respawn(float amount)
+	private IEnumerator Respawn (float duration)
 	{
-		// Instantiate blood particle and relocate the player
+		yield return new WaitForSeconds(duration);
+
 	}
 }
