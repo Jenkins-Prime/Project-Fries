1f61c96afab593a10fc9ebeee2d67cdcd77bc976
joshmond <joshmond@ymail.com>
2016-02-27 02:34:55 +0000

    Updated the world/level select system

--- Assets/Scripts/Hub World/SelectLevel.cs
+++ Assets/Scripts/Hub World/SelectLevel.cs
@@ -4,41 +4,100 @@ using UnityEngine.UI;
 
 public class SelectLevel : MonoBehaviour 
 {
-	private Text level;
+	public GameObject levelSelectOne;
+	public GameObject levelSelectTwo;
+	public GameObject levelSelectThree;
+	public GameObject levelSelectFour;
+	public bool[] isLevelComplete;
+	
+
+	
 	private int currentLevel;
 	private SelectWorld selectWorld;
+	private Text levelOneSelector;
+	private Text levelTwoSelector;
+	private Text levelThreeSelector;
+	private Text levelFourSelector;
+	private Image lockedLevelIcon;
+	private Image castleIcon;
+	private Image bossIcon;
+	private bool isLevelLocked;
+	private Text greyOutLevelText;
+	private Text greyOutText;
+
 
 	void Awake()
 	{
-		level = GameObject.FindGameObjectWithTag ("Level Select").GetComponent<Text> ();
 		selectWorld = GameObject.FindGameObjectWithTag ("World Selector").GetComponent<SelectWorld> ();
+		levelOneSelector = GameObject.FindGameObjectWithTag ("LevelOneSelector").transform.GetChild (1).GetComponent<Text> ();
+		levelTwoSelector = GameObject.FindGameObjectWithTag ("LevelTwoSelector").transform.GetChild (1).GetComponent<Text> ();
+		levelThreeSelector = GameObject.FindGameObjectWithTag ("LevelThreeSelector").transform.GetChild (1).GetComponent<Text> ();
+		levelFourSelector = GameObject.FindGameObjectWithTag ("LevelFourSelector").transform.GetChild (1).GetComponent<Text> ();
+		lockedLevelIcon = GameObject.FindGameObjectWithTag ("LockedLevel").GetComponent<Image> ();
+		castleIcon = GameObject.FindGameObjectWithTag ("Castle Icon").GetComponent<Image> ();
+		bossIcon = GameObject.FindGameObjectWithTag ("Boss Icon").GetComponent<Image> ();
+		greyOutLevelText = GameObject.Find ("Level").GetComponent<Text> ();
+		greyOutText = GameObject.Find ("LevelText").GetComponent<Text> ();
+
+
 	}
 
 	void Start () 
 	{
 		currentLevel = 1;
+		levelSelectOne.SetActive (false);
+		levelSelectTwo.SetActive (false);
+		levelSelectThree.SetActive (false);
+		levelSelectFour.SetActive (false);
+		levelOneSelector.text = currentLevel.ToString ();
+		levelTwoSelector.text = currentLevel.ToString ();
+		levelThreeSelector.text = currentLevel.ToString ();
+		levelFourSelector.text = currentLevel.ToString ();
+		lockedLevelIcon.enabled = false;
+		isLevelLocked = true;
+		castleIcon.enabled = false;
+		bossIcon.enabled = false;
+
+		for(int index = 0; index < isLevelComplete.Length; index++)
+		{
+			isLevelComplete[index] = false;
+		}
 	}
 
 	void Update () 
 	{
-		if(currentLevel > 9)
+
+		if(isLevelLocked)
+		{
+			lockedLevelIcon.enabled = true;
+			greyOutLevelText.color = Color.grey;
+			greyOutText.color = Color.grey;
+		}
+		else
+		{
+			lockedLevelIcon.enabled = false;
+			greyOutLevelText.color = Color.black;
+			greyOutText.color = Color.black;
+
+		}
+
+		if(currentLevel > 10)
 		{
 			currentLevel = 1;
 		}
 
 		if(currentLevel < 1)
 		{
-			currentLevel = 9;
+			currentLevel = 10;
 		}
 
-		if(currentLevel == 1 && Input.GetButtonDown("Submit"))
-		{
-			Application.LoadLevel(1);
-		}
-
+		UnlockLevels ();
+		ShowLevels ();
 		LevelSelect ();
-		level.text = currentLevel.ToString ();
-
+		levelOneSelector.text = currentLevel.ToString ();
+		levelTwoSelector.text = currentLevel.ToString ();
+		levelThreeSelector.text = currentLevel.ToString ();
+		levelFourSelector.text = currentLevel.ToString ();
 	}
 
 	private void LevelSelect()
@@ -51,6 +110,105 @@ public class SelectLevel : MonoBehaviour
 		if(Input.GetKeyDown(KeyCode.DownArrow))
 		{
 			currentLevel--;
+		}
+
+	}
+
+	private void ShowLevels()
+	{
+		if (Input.GetButtonDown ("Submit") && selectWorld.isWorldOne) 
+		{
+			levelSelectOne.SetActive (true);
+			levelSelectTwo.SetActive (false);
+			levelSelectThree.SetActive (false);
+			levelSelectFour.SetActive (false);
+		}
+
+		if(Input.GetButtonDown("Submit") && selectWorld.isWorldTwo)
+		{
+			levelSelectOne.SetActive(false);
+			levelSelectTwo.SetActive(true);
+			levelSelectThree.SetActive (false);
+			levelSelectFour.SetActive(false);
+		}
+
+		if(Input.GetButtonDown("Submit") && selectWorld.isWorldThree)
+		{
+			levelSelectOne.SetActive(false);
+			levelSelectTwo.SetActive(false);
+			levelSelectThree.SetActive (true);
+			levelSelectFour.SetActive(false);
+		}
+
+		if(Input.GetButtonDown("Submit") && selectWorld.isWorldFour)
+		{
+			levelSelectOne.SetActive(false);
+			levelSelectTwo.SetActive(false);
+			levelSelectThree.SetActive (false);
+			levelSelectFour.SetActive(true);
+		}
+	}
+	
+
+	private void UnlockLevels()
+	{
+		if(currentLevel == 1 && selectWorld.isWorldOne)
+		{
+			isLevelLocked = false;
+			
+			if(Input.GetKeyDown(KeyCode.Space))
+			{
+				Application.LoadLevel("testscene");
+			}
+		}
+		else if(currentLevel == 2 && isLevelComplete[0] && selectWorld.isWorldOne)
+		{
+			isLevelLocked = false;
+		}
+		else if(currentLevel == 3 && isLevelComplete[1] && selectWorld.isWorldOne)
+		{
+			isLevelLocked = false;
+		}
+		else if(currentLevel == 4 && isLevelComplete[2] && selectWorld.isWorldOne)
+		{
+			isLevelLocked = false;
+		}
+		else if(currentLevel == 5 && isLevelComplete[3] && selectWorld.isWorldOne)
+		{
+			castleIcon.enabled = true;
+			isLevelLocked = false;
+		}
+		else if(currentLevel == 6 && isLevelComplete[4] && selectWorld.isWorldOne)
+		{
+			isLevelLocked = false;
+		}
+		else if(currentLevel == 7 && isLevelComplete[5] && selectWorld.isWorldOne)
+		{
+			isLevelLocked = false;
+		}
+		else if(currentLevel == 8 && isLevelComplete[6] && selectWorld.isWorldOne)
+		{
+			isLevelLocked = false;
+		}
+		else if(currentLevel == 9 && isLevelComplete[7] && selectWorld.isWorldOne)
+		{
+			isLevelLocked = false;
+		}
+		else if(currentLevel == 10 && isLevelComplete[8] && selectWorld.isWorldOne)
+		{
+			bossIcon.enabled = true;
+			isLevelLocked = false;
+		}
+		else
+		{
+			castleIcon.enabled = false;
+			bossIcon.enabled = false;
+			isLevelLocked = true;
+
+			if(isLevelLocked && Input.GetButtonDown("Jump"))
+			{
+				Debug.Log ("Level Locked!");
+			}
 		}
 	}
 }
--- Assets/Scripts/Hub World/SelectWorld.cs
+++ Assets/Scripts/Hub World/SelectWorld.cs
@@ -1,14 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 
 public class SelectWorld : MonoBehaviour 
 {
-	public GameObject levelSelectOne;
-	public GameObject levelSelectTwo;
-	public GameObject levelSelectThree;
-	public GameObject levelSelectFour;
 
+	public int currentWorld;
 	[HideInInspector]
 	public bool isWorldOne;
 	[HideInInspector]
@@ -17,12 +15,33 @@ public class SelectWorld : MonoBehaviour
 	public bool isWorldThree;
 	[HideInInspector]
 	public bool isWorldFour;
-	
-	public int currentWorld;
+
 	private GameObject worldOne;
 	private GameObject worldTwo;
 	private GameObject worldThree;
 	private GameObject worldFour;
+	
+
+	private Text worldOneText;
+	private Text worldTwoText;
+	private Text worldThreeText;
+	private Text worldFourText;
+
+	private Image WorldOneBackground;
+	private Image WorldTwoBackground;
+	private Image WorldThreeBackground;
+	private Image WorldFourBackground;
+	private Image worldTwoLocked;
+	private Image worldThreeLocked;
+	private Image worldFourLocked;
+
+
+	private bool isWorldTwoUnlocked;
+	private bool isWorldThreeUnlocked;
+	private bool isWorldFourUnlocked;
+
+
+	
 
 	void Awake()
 	{
@@ -30,102 +49,187 @@ public class SelectWorld : MonoBehaviour
 		worldTwo = GameObject.FindGameObjectWithTag("World Two");
 		worldThree = GameObject.FindGameObjectWithTag("World Three");
 		worldFour = GameObject.FindGameObjectWithTag("World Four");
+
+		WorldOneBackground = GameObject.FindGameObjectWithTag("World Names").transform.GetChild(0).GetComponent<Image>();
+		WorldTwoBackground = GameObject.FindGameObjectWithTag("World Names").transform.GetChild(1).GetComponent<Image>();
+		WorldThreeBackground = GameObject.FindGameObjectWithTag("World Names").transform.GetChild(2).GetComponent<Image>();
+		WorldFourBackground = GameObject.FindGameObjectWithTag("World Names").transform.GetChild(3).GetComponent<Image>();
+		worldOneText = GameObject.FindGameObjectWithTag("World Names").transform.GetChild(4).GetComponent<Text>();
+		worldTwoText = GameObject.FindGameObjectWithTag("World Names").transform.GetChild(5).GetComponent<Text>();
+		worldThreeText = GameObject.FindGameObjectWithTag("World Names").transform.GetChild(6).GetComponent<Text>();
+		worldFourText = GameObject.FindGameObjectWithTag("World Names").transform.GetChild(7).GetComponent<Text>();
+
+		worldTwoLocked = GameObject.FindGameObjectWithTag ("Locked World").transform.GetChild (0).GetComponent<Image> ();
+		worldThreeLocked = GameObject.FindGameObjectWithTag ("Locked World").transform.GetChild (1).GetComponent<Image> ();
+		worldFourLocked = GameObject.FindGameObjectWithTag ("Locked World").transform.GetChild (2).GetComponent<Image> ();
+
 	}
 	
 	void Start () 
 	{
 		currentWorld = 1;
-		levelSelectOne.SetActive (false);
-		levelSelectTwo.SetActive (false);
-		levelSelectThree.SetActive (false);
-		levelSelectFour.SetActive (false);
-
+		worldOneText.text = "";
+		worldTwoText.text = "";
+		worldThreeText.text = "";
+		worldFourText.text = "";
 		isWorldOne = false;
 		isWorldTwo = false;
 		isWorldThree = false;
 		isWorldFour = false;
+		WorldOneBackground.enabled = false;
+		WorldTwoBackground.enabled = false;
+		WorldThreeBackground.enabled = false;
+		WorldFourBackground.enabled = false;
+
+		isWorldTwoUnlocked = false;
+		isWorldThreeUnlocked = false;
+		isWorldFourUnlocked = false;
 	}
 
 	void Update () 
 	{
-		WorldSelect ();
+		if(Input.GetKeyDown(KeyCode.A))
+		{
+			isWorldTwoUnlocked = true;
+		}
 
+		WorldSelect ();
+		UnlockWorld ();
 	}
 
 	private void WorldSelect()
 	{
-		if(Input.GetKeyDown(KeyCode.RightArrow))
+		if (Input.GetKeyDown (KeyCode.RightArrow)) 
 		{
 			currentWorld++;
 		}
 
-		if(Input.GetKeyDown(KeyCode.LeftArrow))
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) 
 		{
 			currentWorld--;
 
 		}
 
-		
 		if (currentWorld == 1) 
 		{
+			worldOneText.text = "Grassland";
+			worldTwoText.text = "";
+			worldThreeText.text = "";
+			worldFourText.text = "";
+			isWorldOne = true;
+			isWorldTwo = false;
+			isWorldThree = false;
+			isWorldFour = false;
 
-			if(Input.GetButtonDown ("Submit"))
-			{
-				levelSelectOne.SetActive (true);
-				levelSelectTwo.SetActive (false);
-				levelSelectThree.SetActive (false);
-				levelSelectFour.SetActive (false);
-			}
-			
 			gameObject.transform.position = new Vector3 (worldOne.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
-		}
-		else if (currentWorld == 2) 
+			worldOne.transform.localScale = new Vector3(32, 32, 0);
+			worldFour.transform.localScale = new Vector3(24, 24, 0);
+			worldTwo.transform.localScale = new Vector3(24, 24, 0);
+			WorldOneBackground.enabled = true;
+			WorldTwoBackground.enabled = false;
+			WorldThreeBackground.enabled = false;
+			WorldFourBackground.enabled = false;
+		} 
+		else if (currentWorld == 2 && isWorldTwoUnlocked) 
 		{
-			if(Input.GetButtonDown ("Submit"))
-			{
-				levelSelectOne.SetActive (false);
-				levelSelectTwo.SetActive (true);
-				levelSelectThree.SetActive (false);
-				levelSelectFour.SetActive (false);
-			}
+			worldOneText.text = "";
+			worldTwoText.text = "Candy World";
+			worldThreeText.text = "";
+			worldFourText.text = "";
+
+			isWorldOne = false;
+			isWorldTwo = true;
+			isWorldThree = false;
+			isWorldFour = false;
 			
-			gameObject.transform.position = new Vector3(worldTwo.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
-		}
-		else if (currentWorld == 3) 
+			gameObject.transform.position = new Vector3 (worldTwo.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+			worldOne.transform.localScale = new Vector3(24, 24, 0);
+			worldTwo.transform.localScale = new Vector3(32, 32, 0);
+			worldThree.transform.localScale = new Vector3(24, 24, 0);
+			WorldTwoBackground.enabled = true;
+			WorldOneBackground.enabled = false;
+			WorldThreeBackground.enabled = false;
+			WorldFourBackground.enabled = false;
+		} 
+		else if (currentWorld == 3 && isWorldThreeUnlocked) 
 		{
-			if(Input.GetButtonDown ("Submit"))
-			{
-				isWorldThree = true;
-				levelSelectOne.SetActive (false);
-				levelSelectTwo.SetActive (false);
-				levelSelectThree.SetActive (true);
-				levelSelectFour.SetActive (false);
-			}
-			
-			gameObject.transform.position = new Vector3(worldThree.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
-		}
-		else 
+			worldOneText.text = "";
+			worldTwoText.text = "";
+			worldThreeText.text = "Sewer City";
+			worldFourText.text = "";
+			isWorldOne = false;
+			isWorldTwo = false;
+			isWorldThree = true;
+			isWorldFour = false;
+	
+			gameObject.transform.position = new Vector3 (worldThree.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+			worldTwo.transform.localScale = new Vector3(24, 24, 0);
+			worldThree.transform.localScale = new Vector3(32, 32, 0);
+			worldFour.transform.localScale = new Vector3(24, 24, 0);
+			WorldThreeBackground.enabled = true;
+			WorldOneBackground.enabled = false;
+			WorldTwoBackground.enabled = false;
+			WorldFourBackground.enabled = false;
+		} 
+		else if (currentWorld == 4 && isWorldFourUnlocked)
 		{
-			if(Input.GetButtonDown ("Submit"))
-			{
-				isWorldFour = true;
-				levelSelectOne.SetActive (false);
-				levelSelectTwo.SetActive (false);
-				levelSelectThree.SetActive (false);
-				levelSelectFour.SetActive (true);
-			}
-			
-			gameObject.transform.position = new Vector3(worldFour.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+			worldOneText.text = "";
+			worldTwoText.text = "";
+			worldThreeText.text = "";
+			worldFourText.text = "Abyss";
+			isWorldOne = false;
+			isWorldTwo = false;
+			isWorldThree = false;
+			isWorldFour = true;
+
+			gameObject.transform.position = new Vector3 (worldFour.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+			worldThree.transform.localScale = new Vector3(24, 24, 0);
+			worldFour.transform.localScale = new Vector3(32, 32, 0);
+			WorldFourBackground.enabled = true;
+			WorldOneBackground.enabled = false;
+			WorldTwoBackground.enabled = false;
+			WorldThreeBackground.enabled = false;
+
 		}
 		
-		if(currentWorld > 4)
+		if (currentWorld > 4) 
 		{
 			currentWorld = 1;
 		}
 		
-		if(currentWorld < 1)
+		if (currentWorld < 1) 
 		{
 			currentWorld = 4;
+		}
+	}
+
+	private void UnlockWorld()
+	{
+		if(!isWorldTwoUnlocked)
+		{
+			worldTwoLocked.enabled = true;
+		}
+		else
+		{
+			worldTwoLocked.enabled = false;
+		}
+		
+		if(!isWorldThreeUnlocked)
+		{
+			worldThreeLocked.enabled = true;
+		}
+		else
+		{
+			worldThreeLocked.enabled = false;
+		}
+		
+		if(!isWorldFourUnlocked)
+		{
+			worldFourLocked.enabled = true;
+		}
+		else
+		{
+			worldFourLocked.enabled = false;
 		}
 	}
 }
